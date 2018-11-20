fd294f7aa931e199fa40de7a7b3b190098fefa9d
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-11-20 17:15:49 -0500

    add keys for google auth and mongo db

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+module.exports = {
+    google: {
+        clientID: '450456877793-7mpe52l15fmvpml9c2tnhnbutk6j4csb.apps.googleusercontent.com',
+        clientSecret: 'y7XxJij0rQXgCl4kG9KZpkTB'
+    },
+    mongodb:
+    {
+        dbURI:'mongodb://testoauthadmin:password1@ds157923.mlab.com:57923/test-oauth-db'
+    },
+    session:
+    {
+        cookieKey: 'ILoveJavaScript Itis Awesome'
+    }
+};
+
