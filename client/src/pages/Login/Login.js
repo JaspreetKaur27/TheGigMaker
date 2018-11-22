48bee2040f6f66bf3453b8bf4b73bb7fabb8faf7
jessalynchen <jessalynchen@gmail.com>
2018-11-22 18:31:20 -0500

    add description

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -5,61 +5,65 @@ import Dashboard from '../Dashboard';
 import API from "../../utils/API";
 import Navbar from "../../components/Navbar";
 
-
-
 class Login extends Component {
 
     constructor(props) {
         super(props);
         this.getUser = this.getUser.bind(this);
         this.state = {
-          isLoggedIn: false,
-          user: []
+            isLoggedIn: false,
+            user: []
         }
-    
-      }
 
-      componentDidMount()
-      {
-          this.getUser();
-      }
+    }
 
-      getUser = () => {
+    componentDidMount() {
+        this.getUser();
+    }
+
+    getUser = () => {
         API.getProfile()
-        .then(res => {
-            console.log(res);
-            this.setState({
-              user: res.data
-            })
-        }).catch(err => console.log(err));
-      }
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    user: res.data
+                })
+            }).catch(err => console.log(err));
+    }
 
-     
+
     render() {
         return (
-             <Router>
-            <div>   
-            <Navbar>
-            { !this.state.isLoggedIn ?
-            <a className="navbar-brand" href="/">
-              Login
-            </a>
-            : 
-            [<a className="navbar-brand" href="/dashboard">
-              Dashboard
-          </a>,
-            <a className="navbar-brand" href="/AddProject">
-              Create New Gig
-          </a>,
-            <a className="navbar-brand" href="/">
-              Logout
-          </a>] }
-          </Navbar> 
+            <Router>
+                <div>
+                    <Navbar>
+                        {!this.state.isLoggedIn ?
+                            <a className="navbar-brand" href="/">
+                                Login
+                            </a>
+                            :
+                            [<a className="navbar-brand" href="/dashboard">
+                                Dashboard
+                            </a>,
+                            <a className="navbar-brand" href="/AddProject">
+                                Create New Gig
+                            </a>,
+                            <a className="navbar-brand" href="/">
+                                Logout
+                            </a>]}
+                    </Navbar>
                     <Switch>
-                        <Route  exact path="/dashboard" component={Dashboard} />
-                    </Switch>             
-                <Button><a href="http://localhost:3001/api/auth/google">Sign in with Google+</a></Button>
-            </div>
+                        <Route exact path="/dashboard" component={Dashboard} />
+                    </Switch>
+                    <div>
+                        <h1>Welcome to theGigMaker!</h1>
+                        <p><b>theGigMaker</b> is a web-based application that allows users to find short-term gigs based on their hobbies or specialities.
+                        Login to explore and connect with fellow users.
+                    </p>
+                    </div>
+
+                    <Button><a href="http://localhost:3001/api/auth/google">Sign in with Google+</a></Button>
+                </div>
             </Router>
         )
     }
