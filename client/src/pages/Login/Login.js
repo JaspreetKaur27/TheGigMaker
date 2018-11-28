7588de4dbac5b22492052c7edb296c6e8b628ef5
jessalynchen <jessalynchen@gmail.com>
2018-11-27 19:38:45 -0500

    installed npm react-bootstrap

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -5,7 +5,7 @@ import Dashboard from '../Dashboard';
 import API from "../../utils/API";
 import Navbar from "../../components/Navbar";
 import "./Login.css"
-
+{/*npm install React Bootstrap Library use this command $ npm install react-bootstrap*/}
 class Login extends Component {
 
     constructor(props) {
@@ -58,14 +58,28 @@ class Login extends Component {
                     </Switch>
                     
 
-                    <div className="site-background">
+                    <div className="site-background" >
+                        {/* <Carousel>
+                            <Carousel.Item>
+                            <img width={900} height={500} alt="900x500" src="https://kumdotv.com/images/financial-clipart-financial-analysis-4.png"/>
+                            <Carousel.Caption>
+                                <h3>Work Remotely</h3>
+                            </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img width={900} height={500} alt="900x500" src="https://kumdotv.com/images/finance-clipart-9.png"/>
+                                <Carousel.Caption>
+                                    <h3>Collaborate</h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>; */}
+
                         <h1 className="title">Welcome to theGigMaker!</h1>
                         <p className="subtitle"><b>theGigMaker</b> is a web-based application that allows 
                         <p>users to find short-term gigs based on their hobbies or specialities.</p>
                         <Button><a href="http://localhost:3001/api/auth/google">Sign in with Google+</a></Button>
                     </p>
-                    <div></div>
-                    
+                                        
                     {/* <img src="https://clip2art.com/images/sky-clipart-background-8.jpg" alt="office-background"/> */}
                     <video id="background-video" loop autoPlay>
                         <source src="https://vimeo.com/92688656" type="video"/>
