cca40241eccb289118244bcf6e02af66ea6069e2
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-12-03 14:03:25 -0500

    fixing to make it responsive

--- client/src/components/Column/Column.js
+++ client/src/components/Column/Column.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-// Column used to responsively display each player in the card-deck
 const Column = props => {
   return (
     <div className="col-lg-3 col-md-4 col-sm-6">
