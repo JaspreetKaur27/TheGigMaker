8b7d412bd8bc8e75eb05b296ac55d7ad0d9a815e
Mjay <martin_jarrin@hotmail.com>
2018-12-11 16:11:17 -0500

    test5

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 registerServiceWorker();
